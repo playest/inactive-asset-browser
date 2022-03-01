977df70092489fa675e53c9b09859ab5e051f3af
playest <playest.ff@laposte.net>
2022-03-01 20:16:44 +0100

    show every scene from relevant inactive module

---
+++ README.md
@@ -0,0 +1,3 @@
+# Inactive Asset Browwer
+
+I manage hundreds of modules, each of them containing dozens of maps. I can't activate all those modules because then the world takes a lot of time to load. What I want to do is browse their content and activate each module when neeeded. This is the goals of this module.
---
+++ scripts/module.ts
@@ -0,0 +1,42 @@
+// Set this variables as their initilized state, see doc of LenientGlobalVariableTypes for an explanation
+interface LenientGlobalVariableTypes {
+    game: never;
+    socket: never;
+    ui: never;
+    canvas: never;
+}
+
+Hooks.once('init', async function() {
+
+});
+
+function isOfInterest(moduleName: string): boolean {
+    return ["czepeku-maps-megapack"].includes(moduleName);
+}
+
+Hooks.once('ready', async function() {
+    console.log("inactive-asset-browser started");
+    let a = game.modules;
+    for(const [name, module] of game.modules.entries()) {
+        if(isOfInterest(name)) {
+            for(const pack of module.packs) {
+                if(pack.type == "Scene") {
+                    //console.log(pack.absPath);
+                    const url = "modules/" + module.id + "/" + pack.path;
+                    console.log(url);
+                    const r = await fetch(url);
+                    const text = await r.text();
+                    const lines = text.split("\n");
+                    console.log("lines", lines.length);
+                    for(const line of lines) {
+                        if(line !== "") {
+                            const fr = new FileReader();
+                            const o = JSON.parse(line);
+                            console.log(o);
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
