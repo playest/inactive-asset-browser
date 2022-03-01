80fab404fe3ab3992eb791e33b90323063b2c975
playest <playest.ff@laposte.net>
2022-03-01 21:14:44 +0100

    show a window and skip most of the scenes to be faster for testing

--- scripts/module.ts
+++ scripts/module.ts
@@ -6,6 +6,12 @@ interface LenientGlobalVariableTypes {
     canvas: never;
 }
 
+const MODULE_NAME = "inactive-asset-browser";
+const PATH_TO_ROOT_OF_MODULE = `modules/${MODULE_NAME}/`;
+function joinPath(...paths: string[]) {
+    return paths.join("/");
+}
+
 Hooks.once('init', async function() {
 
 });
@@ -14,12 +20,32 @@ function isOfInterest(moduleName: string): boolean {
     return ["czepeku-maps-megapack"].includes(moduleName);
 }
 
+async function showMainWindow() {
+    const rendered_html = await renderTemplate(joinPath(PATH_TO_ROOT_OF_MODULE, "templates/assetList.html"), {});
+    let d = new Dialog({
+        title: "MyDialogTitle",
+        content: rendered_html,
+        buttons: {
+            toggle: {
+                icon: '<i class="fas fa-check"></i>',
+                label: "Okay",
+                callback: () => console.log("Okay")
+            },
+        },
+        default: "toggle",
+        close: html => {
+            console.log(html);
+        },
+      });
+    d.render(true);
+}
+
 Hooks.once('ready', async function() {
     console.log("inactive-asset-browser started");
-    let a = game.modules;
+    let i = 0;
     for(const [name, module] of game.modules.entries()) {
         if(isOfInterest(name)) {
-            for(const pack of module.packs) {
+            for(const pack of module.packs.slice(0, 3)) { // TODO this slice is just for quick testing
                 if(pack.type == "Scene") {
                     //console.log(pack.absPath);
                     const url = "modules/" + module.id + "/" + pack.path;
@@ -30,7 +56,6 @@ Hooks.once('ready', async function() {
                     console.log("lines", lines.length);
                     for(const line of lines) {
                         if(line !== "") {
-                            const fr = new FileReader();
                             const o = JSON.parse(line);
                             console.log(o);
                         }
@@ -38,5 +63,11 @@ Hooks.once('ready', async function() {
                 }
             }
         }
+        // TODO this is just for quick testing
+        if(i > 10) {
+            break;
+        }
+        i++;
     }
+    showMainWindow();
 });
